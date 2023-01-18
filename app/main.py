c84a1b88c4f1f53a4c4f661057bbe00dcbc090c8
Manh Pham <pvm@MacBook-Pro-cua-Manh.local>
2023-01-18 11:28:35 +0700

    add CORS

--- app/main.py
+++ app/main.py
@@ -7,24 +7,27 @@ from loguru import logger
 
 # API
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.param_functions import Depends
 from typing import List, Dict
-
+from pydantic import BaseModel
 
 import logging
 
 # Start server
 app = FastAPI(host = configs.host_server, port = configs.port_server)
+# Setup CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Setup logging
 handler = logging.FileHandler(filename='logs/file_log.log')
 logger.add(handler)
-
-from pydantic import BaseModel
-
-app = FastAPI()
-
-
 
 # generator module
 tetwish_generator = TetWishGenerator(config = configs, timeout=configs.timeout)
