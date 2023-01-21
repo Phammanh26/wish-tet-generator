ef3b87ffd157cd7b36bde34aed8c4e0703436354
Ubuntu <ubuntu@ip-172-31-5-183.ap-southeast-1.compute.internal>
2023-01-21 06:06:56 +0000

    mergae conflict

--- endpoint.sh
+++ endpoint.sh
@@ -1,3 +1,7 @@
+
 # script to run the server
+## run server
 kill -9 $(lsof -t -i:8000)
+#uvicorn server:app --reload --host 0.0.0.0 --port 8000
+
 uvicorn app.main:app --reload --host 0.0.0.0  --port 8000
